8fe02be92cf1d0fbe412b915362c88137f3b6224
Mahmoud-Da <mdabbagh6@gmail.com>
2023-07-14 17:16:11 +0900

    finish lesson 3(7)

--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,3 +1,5 @@
+import { MouseEvent, useState } from "react";
+
 function ListGroup2() {
   return (
     // empty brackets represents Fragment Element
@@ -9,6 +11,55 @@ function ListGroup2() {
         <li className="list-group-item">A third item</li>
         <li className="list-group-item">A fourth item</li>
         <li className="list-group-item">And a fifth one</li>
+      </ul>
+    </>
+  );
+}
+
+function ListGroup3() {
+  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  // better way to achive the same result of condition (if we don't have to use parameters)
+  // a better logic than using null
+  // true && "moody" => "moody"
+  // false && "moody" => false (nothing will return)
+  const message = items.length === 0 && <p>No item Found</p>;
+  // or even store the logic inside a function to be able to use parameter if we want
+  const getMessage = (parameter: string) => {
+    return items.length === 0 ? <p>No item found{parameter}</p> : null;
+    // return items.length === 0 && <p>No item found{parameter}</p>
+  };
+  // add condition
+  //   if (items.length === 0) {
+  //     return (
+  //       <>
+  //         <h1>List</h1>;<p>No item found</p>
+  //       </>
+  //     );
+  //   }
+
+  // handler click
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  // active only the selected index -1 means nothing selected
+  let selectedIndex = -1;
+
+  return (
+    // empty brackets represents Fragment Element
+    <>
+      <h1>List</h1>
+      {message}
+      <ul className="list-group">
+        {items.map((item) => (
+          // key option to let React to keep tracking of our element(removing, editing, and so...)
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={handleClick}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
@@ -35,15 +86,32 @@ function ListGroup() {
   //       </>
   //     );
   //   }
+
+  // handler click
+  const handleClick = (event: MouseEvent) => console.log(event);
+
+  // active only the selected index -1 means nothing selected
+  // Hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     // empty brackets represents Fragment Element
     <>
       <h1>List</h1>
       {message}
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           // key option to let React to keep tracking of our element(removing, editing, and so...)
-          <li className="list-group-item" key={item}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
