05113629849ac448fd8dc1a756f73206fc43d5f7
Mahmoud-Da <mdabbagh6@gmail.com>
2023-07-18 16:18:36 +0900

    finish lesson 3(11) passing children

--- README.md
+++ README.md
@@ -9,3 +9,15 @@ to run a react application
 npm run dev / sudo npm run dev
 
 to install a package to react application we use node package manger (npm)
+
+Extintions:
+
+- Name: ES7+ React/Redux/React-Native snippets
+  Id: dsznajder.es7-react-js-snippets
+  Description: Extensions for React, React-Native and Redux in JS/TS with ES7+ syntax. Customizable. Built-in integration with prettier.
+  Version: 4.4.3
+  Publisher: dsznajder
+  VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=dsznajder.es7-react-js-snippets
+
+we can type [rafce] (a shortcut to create a component and export it)
+(name shortcut means => react arrow function export component)
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,3 +1,4 @@
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
 
 function App() {
@@ -8,13 +9,25 @@ function App() {
   };
 
   return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cites"
-        onSlecetItem={handleSelectItem}
-      />
-    </div>
+    <>
+      <div>
+        <ListGroup
+          items={items}
+          heading="Cites"
+          onSlecetItem={handleSelectItem}
+        />
+      </div>
+
+      <div>
+        {/* if we want to type Hello World inside the Alert tag like normal HTML element we use (children Props) */}
+        {/* <Alert text="Hello World" /> */}
+        <Alert>Hello World</Alert>
+        {/* using React node class we can pass element to Alert */}
+        <Alert>
+          Hello <span>World</span>
+        </Alert>
+      </div>
+    </>
   );
 }
 
