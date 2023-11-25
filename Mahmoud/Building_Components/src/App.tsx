16dc58d3e19f5f679352a8a84c8f521862b06e44
Mahmoud-Da <mdabbagh6@gmail.com>
2023-11-25 15:00:26 +0900

    finsih from 1-5

---
+++ Mahmoud/Building_Components/src/App.tsx
@@ -0,0 +1,21 @@
+import ListGroup1 from "./components/ListGroup1";
+import ListGroup2 from "./components/ListGroup2";
+import ListGroup3 from "./components/ListGroup3";
+
+function App() {
+  return (
+    <>
+      {/* bootstrap */}
+      <ListGroup1 />
+
+      {/* rendering list */}
+      <ListGroup2 />
+
+      {/* conditional rendering */}
+      <ListGroup3 />
+    </>
+  );
+}
+export default App;
+
+<h1>List</h1>;
