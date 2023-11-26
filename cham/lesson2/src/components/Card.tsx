d1bb0dc3bfe401dbcdfa0dd0fb38d186ef5bb1c2
Mahmoud-Da <mdabbagh6@gmail.com>
2023-11-26 16:13:42 +0900

    finish lesseon2_2

---
+++ cham/lesson2/src/components/Card.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import lili from "../assets/lili.png";
+const Card = () => {
+  const [selectedButton, setSelectedButtton] = useState(-1);
+
+  // ボタンのスタイルを戻したい
+  // handle => clickの処理が変数に保存する際に利用する単語
+  const handleButtonClick = () => {
+    setSelectedButtton(selectedButton === 0 ? -1 : 0);
+  };
+  return (
+    <div className="card">
+      <img src={lili} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+        <a
+          href="#"
+          className={
+            selectedButton === 0 ? "btn btn-primary" : "btn btn-secondary"
+          }
+          // onClick={() => setSelectedIndex(0)}
+          onClick={handleButtonClick}
+        >
+          Go somewhere
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
