850683b6c09c5f59b51ad66b7de1360d10a9d9ae
cham1235 <hana171819@gmail.com>
2023-12-10 19:17:52 +0900

    完成

--- cham/practice/src/App.tsx
+++ cham/practice/src/App.tsx
@@ -1,8 +1,9 @@
 // import Cards from "./components/Cards";
 
 import Alert from "./components/Alert";
-import ListGroups1 from "./components/ListGroup1";
-import ListGroups2 from "./components/ListGroup1";
+// import ListGroups1 from "./components/ListGroup1";
+// import ListGroups2 from "./components/ListGroup1";
+import ListGroupV from "./components/ListGroupV";
 
 function App() {
   let t_flowers: string[] = [
@@ -14,20 +15,20 @@ function App() {
     "Sunflower",
   ];
 
-  let t_fruits: string[] = [
-    "Cherris",
-    "Watermelon",
-    "Avocado",
-    "Fig",
-    "Plum",
-    "Peach",
-  ];
+  // let t_fruits: string[] = [
+  //   "Cherris",
+  //   "Watermelon",
+  //   "Avocado",
+  //   "Fig",
+  //   "Plum",
+  //   "Peach",
+  // ];
   const handleSlecetedItem = (item: string) => {
     console.log(item);
   };
   return (
     <>
-      {/* <Cards /> */}
+      {/* <Cards />
       <ListGroups1
         items={t_flowers}
         header="The flowers"
@@ -44,7 +45,13 @@ function App() {
         Hello <span>Lily</span>
         <br></br>
         <span>My cat</span>
-      </Alert>
+      </Alert> */}
+      <ListGroupV
+        items={t_flowers}
+        header="The flowers"
+        style="list-group-item list-group-item-primary"
+        onSlecetedItem={handleSlecetedItem}
+      />
     </>
   );
 }
