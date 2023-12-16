930a7d113fb6afa1402895cf2aeab6222d992ce5
Mahmoud-Da <mdabbagh6@gmail.com>
2023-12-16 16:22:45 +0900

    keeping component pure

---
+++ Managing-Component-State/src/App.tsx
@@ -0,0 +1,18 @@
+import ButtonShow1 from "./components/ButtonShow1";
+import FullName from "./components/FullName";
+import Massege from "./components/Massege";
+
+function App() {
+  return (
+    <>
+      <ButtonShow1 />
+      <FullName />
+      {/*  we got the markup */}
+      <Massege />
+      <Massege />
+      <Massege />
+    </>
+  );
+}
+
+export default App;
