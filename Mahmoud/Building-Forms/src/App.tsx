bc4587bb4b031418368c17faee06654c2e4eaa43
Mahmoud-Da <mdabbagh6@gmail.com>
2023-12-23 14:50:43 +0900

    finish Handling_Form_Submission

---
+++ Mahmoud/Building-Forms/src/App.tsx
@@ -0,0 +1,14 @@
+import Form1 from "./components/Form1";
+import Form2 from "./components/Form2";
+
+function App() {
+  return (
+    <>
+      {/* Building_a_Form */}
+      <Form1 />
+
+      <Form2 />
+    </>
+  );
+}
+export default App;
