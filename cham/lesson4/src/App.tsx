4d17f3891c9786b603ea735f4b183a0ffb93e437
Mahmoud-Da <mdabbagh6@gmail.com>
2023-12-24 14:46:51 +0900

    レーソン4完了

--- cham/lesson4/src/App.tsx
+++ cham/lesson4/src/App.tsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import ButtonShow from "./components/ButtonShow";
+import Cart from "./components/Cart";
 import FullName from "./components/FullName";
 import Massege from "./components/Massege";
+import NavBar from "./components/NavBar";
 import UpdateArray from "./components/UpdateArray";
 import UpdateNastedArray from "./components/UpdateNastedArray";
 import UpdateNastedObject from "./components/UpdateNastedObject";
 import UpdateObject from "./components/UpdateObject";
 
 function App() {
+  const [cartItem, setCartItem] = useState([
+    "東京",
+    "大阪",
+    "京都",
+    "北海道",
+    "沖縄",
+  ]);
   return (
     <>
       {/* <ButtonShow /> */}
@@ -21,7 +31,16 @@ function App() {
 
       {/* <UpdateNastedObject /> */}
       {/* <UpdateArray /> */}
-      <UpdateNastedArray />
+      {/* <UpdateNastedArray /> */}
+
+      <NavBar cartItemCount={cartItem.length} />
+      <Cart
+        cartItems={cartItem}
+        onClear={() => setCartItem([])}
+        removeOneItem={() =>
+          setCartItem((cartItem) => [...cartItem.slice(0, -1)])
+        }
+      />
     </>
   );
 }
