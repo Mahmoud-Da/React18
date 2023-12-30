bed2c46812cb0ff4d36c073e9d93776840e8f630
cham1235 <hana171819@gmail.com>
2023-12-30 16:05:48 +0900

    完成

---
+++ cham/practice-ls4/src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+interface Props {
+  cartItemCount: number;
+}
+const NavBar = ({ cartItemCount }: Props) => {
+  return <div>NavBar:{cartItemCount}</div>;
+};
+
+export default NavBar;
