951f118cff430386652db54e2170acf8210c471f
Mahmoud-Da <mdabbagh6@gmail.com>
2024-01-02 20:16:17 +0900

    finish ExpenseFilter

--- Mahmoud/Building-Forms/src/expense-tracker/components/versions/v1/App.tsx
+++ Mahmoud/Building-Forms/src/expense-tracker/components/versions/v1/App.tsx
@@ -8,10 +8,8 @@ import Form6 from "./components/Form6";
 import Form7 from "./components/Form7";
 import Form8 from "./components/Form8";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     {
       id: 1,
@@ -38,10 +36,6 @@ function App() {
       category: "Utilities",
     },
   ]);
-
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
   return (
     <>
       {/* Building_a_Form */}
@@ -71,13 +65,8 @@ function App() {
       <br />
 
       <h1>ExpenseList</h1>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectedCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
       <ExpenseList
-        expenses={visibleExpenses}
+        expenses={expenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
       <br />
