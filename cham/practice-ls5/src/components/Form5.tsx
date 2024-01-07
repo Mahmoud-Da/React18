5a4e2bc91e021da92ab6632772045c031f5226ee
cham1235 <hana171819@gmail.com>
2024-01-07 22:31:51 +0900

    完成

---
+++ cham/practice-ls5/src/components/Form5.tsx
@@ -0,0 +1,40 @@
+import { FieldValues, useForm } from "react-hook-form";
+
+const Form5 = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data: FieldValues) => console.log(data);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            {...register("name")}
+            id="name"
+            type="text"
+            className="form-control"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="age" className="form-label">
+            Age
+          </label>
+          <input
+            {...register("age")}
+            id="age"
+            type="number"
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-success" type="submit">
+          Submit
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default Form5;
