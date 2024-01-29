0c38a57780bab6acb034da9723fc66ecd08ac7a1
cham1235 <hana171819@gmail.com>
2024-01-29 22:27:03 +0900

    完成

--- cham/project2/src/App.tsx
+++ cham/project2/src/App.tsx
@@ -67,7 +67,11 @@ function App() {
   return (
     <>
       <div className="mb-3">
-        <ExpenseForm />
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
       </div>
       <div className="mb-3">
         <ExpenseFilter
