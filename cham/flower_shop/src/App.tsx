d7152e60ad83b83d068f50884c7c90912bd0ef60
cham1235 <hana171819@gmail.com>
2024-02-02 23:25:33 +0900

    修正

--- cham/flower_shop/src/App.tsx
+++ cham/flower_shop/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Navbar } from "react-bootstrap";
+import Navbar from "../src/components/Navbar";
 import style from "../src/App.css";
 function App() {
   return (
