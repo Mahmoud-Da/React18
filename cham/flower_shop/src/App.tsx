0727c98a4a596a7ee2bb1e284ea88ba691ddcc77
cham1235 <hana171819@gmail.com>
2024-02-06 21:54:24 +0900

    完成

--- cham/flower_shop/src/App.tsx
+++ cham/flower_shop/src/App.tsx
@@ -1,10 +1,13 @@
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Area from "./components/Area";
+
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Area />
     </>
   );
 }
