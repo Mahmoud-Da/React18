860261475f45b26f05e90f2b8a7db9c7e8d5c7d5
Mahmoud-Da <mdabbagh6@gmail.com>
2024-02-10 16:51:31 +0900

    finish Understanding_the_Effect_Hook

--- Mahmoud/Connecting-to-the-Backend/src/App.tsx
+++ Mahmoud/Connecting-to-the-Backend/src/App.tsx
@@ -1,9 +1,24 @@
-import Lesson1 from "./components/Understanding_the_Effect_Hook/lesson1";
+import { useState } from "react";
+import Lesson1 from "./components/Understanding_the_Effect_Hook/Lesson1";
+import Lesson2 from "./components/Understanding_the_Effect_Hook/Lesson2";
+import ProductList from "./components/Understanding_the_Effect_Hook/ProductList";
 
 function App() {
+  const [category, setCategory] = useState("");
   return (
     <>
       <Lesson1 />
+      <Lesson2 />
+
+      <select
+        className="form-select"
+        onChange={(event) => setCategory(event.target.value)}
+      >
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+      </select>
+      <ProductList category={category} />
     </>
   );
 }
