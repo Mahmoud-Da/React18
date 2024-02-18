cc3020ad01c44552d2defa2d41e791ef9c9e2725
Mahmoud-Da <mdabbagh6@gmail.com>
2024-02-18 11:25:04 +0900

    finish _Understanding_the_Effect_Hook

---
+++ cham/lesson6/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Lesson2 from "./components/Lesson2";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <Lesson2 />
+      </h1>
+    </>
+  );
+}
+
+export default App;
