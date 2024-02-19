3e0f22c9625b4817ad3579c7884fe2522ce1fb05
cham1235 <hana171819@gmail.com>
2024-02-19 22:45:25 +0900

    完成

---
+++ cham/practice-ls6/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import Lesson1 from "./components/Lesson1";
+
+function App() {
+  return (
+    <>
+      <Lesson1 />
+    </>
+  );
+}
+
+export default App;
