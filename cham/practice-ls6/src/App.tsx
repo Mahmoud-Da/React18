edc7162ea568b746c2b9f45b769ce0720c902eb2
cham1235 <hana171819@gmail.com>
2024-02-20 22:38:13 +0900

    完成

--- cham/practice-ls6/src/App.tsx
+++ cham/practice-ls6/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import Lesson1 from "./components/Lesson1";
+import Lesson2 from "./components/Lesson2";
 
 function App() {
   return (
     <>
       <Lesson1 />
+      <Lesson2 />
     </>
   );
 }
