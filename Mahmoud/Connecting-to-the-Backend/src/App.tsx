4badbffbb32dbc0efd842883478c8f34ecbea2ea
Mahmoud-Da <mdabbagh6@gmail.com>
2024-02-21 09:56:15 +0900

    finish Showing a Loading Indicator

--- Mahmoud/Connecting-to-the-Backend/src/App.tsx
+++ Mahmoud/Connecting-to-the-Backend/src/App.tsx
@@ -8,12 +8,13 @@ import UnderstandingHttpRequests from "./components/6-_Understanding_HTTP_Reques
 import HandlingErrors from "./components/7-_Handling_Errors/HandlingErrors";
 import WorkingWithAsyncAndAwait from "./components/8-_Working_with_Async_and_Await/WorkingWithAsyncAndAwait";
 import CancellingAFetchRequest from "./components/9-_Cancelling_a_Fetch_Request/CancellingAFetchRequest";
+import ShowingALoadingIndicator from "./components/10-_Showing_a_Loading_Indicator/ShowingALoadingIndicator";
 
 function App() {
   const [category, setCategory] = useState("");
   return (
     <>
-      <h4>2-Understanding_the_Effect_Hook</h4>
+      {/* <h4>2-Understanding_the_Effect_Hook</h4>
       <Lesson1 />
       <Lesson2 />
 
@@ -44,7 +45,10 @@ function App() {
       <WorkingWithAsyncAndAwait />
 
       <h4>9-_Cancelling_a_Fetch_Request</h4>
-      <CancellingAFetchRequest />
+      <CancellingAFetchRequest /> */}
+
+      <h4>10-_Showing_a_Loading_Indicator</h4>
+      <ShowingALoadingIndicator />
     </>
   );
 }
