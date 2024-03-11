4443336495a9418e270cafa0f8424b070a53e290
Mahmoud-Da <mdabbagh6@gmail.com>
2024-03-12 05:39:29 +0900

    not finished (5:44)

---
+++ Mahmoud/Connecting-to-the-Backend/src/components/15-_Extracting_the_User_Service/ExtractingTheUserService.tsx
@@ -0,0 +1,108 @@
+import apiClient, { CanceledError } from "../../services/api-client";
+import React, { useEffect, useState } from "react";
+import userService, { User } from "../../services/user-service";
+
+const ExtractingTheUserService = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const { request, cancel } = userService.getAllUsers();
+    request
+      .then((res) => {
+        setUsers(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => cancel();
+  }, []);
+
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    apiClient
+      .delete("https://jsonplaceholder.typicode.com/users/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser: User = {
+      id: 0,
+      name: "Lili",
+      username: "",
+      email: "lili123@gmial.com",
+    };
+    setUsers([newUser, ...users]);
+
+    apiClient
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      // .then((res) => setUsers([res.data, ...users]))
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    apiClient
+      .patch(
+        "https://jsonplaceholder.typicode.com/users/" + user.id,
+        updatedUser
+      )
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div className="">
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger mx-1"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ExtractingTheUserService;
---
+++ Mahmoud/Connecting-to-the-Backend/src/services/user-service.ts
@@ -0,0 +1,19 @@
+import apiClient from "./api-client";
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+class UserService {
+  getAllUsers() {
+    const controller = new AbortController();
+
+    const request = apiClient.get<User[]>("/users", {
+      signal: controller.signal,
+    });
+    return { request, cancel: () => controller.abort() };
+  }
+}
+
+export default new UserService();
