f0d8f8a76a193566b62481de5f913848e0114e25
Mahmoud-Da <mdabbagh6@gmail.com>
2024-04-19 04:57:09 +0900

    finish 17_Creating_a_Custom_Data_Fetching_Hook

---
+++ Mahmoud/Connecting-to-the-Backend/src/components/17-_Creating_a_Custom_Data_Fetching_Hook/CreatingACustomDataFetchingHook.tsx
@@ -0,0 +1,81 @@
+import { CanceledError } from "../../services/api-client";
+import { useEffect, useState } from "react";
+import userService, { User } from "../../services/user-service";
+import useUsers from "../../hooks/useUsers";
+
+const CreatingACustomDataFetchingHook = () => {
+  const { users, error, isLoading, setUsers, setError } = useUsers();
+  const deleteUser = (user: User) => {
+    const originalUsers = [...users];
+    setUsers(users.filter((u) => u.id !== user.id));
+
+    userService.delete(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser: User = {
+      id: 0,
+      name: "Lili",
+      username: "",
+      email: "lili123@gmial.com",
+    };
+    setUsers([newUser, ...users]);
+
+    userService
+      .createUser(newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
+    userService.update(updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
+  return (
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      {isLoading && <div className="spinner-border"></div>}
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
+      <ul className="list-group">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
+            {user.name}
+            <div className="">
+              <button
+                className="btn btn-outline-secondary mx-1"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger mx-1"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default CreatingACustomDataFetchingHook;
