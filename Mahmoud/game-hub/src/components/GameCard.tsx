4a2c6a8bf4033a2670ac1dac853215ac552d47ed
Mahmoud-Da <mdabbagh6@gmail.com>
2024-05-28 11:21:47 +0900

    finish 13-_Displaying_Critic_Score

--- Mahmoud/game-hub/src/components/GameCard.tsx
+++ Mahmoud/game-hub/src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Game } from "../hooks/useGames";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatfromIconList from "./PlatfromIconList";
+import CriticScore from "./CriticScore";
 
 interface Props {
   game: Game;
@@ -13,9 +14,12 @@ const GameCard = ({ game }: Props) => {
         <Image src={game.background_image} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
-          <PlatfromIconList
-            platform={game.parent_platforms.map((p) => p.platform)}
-          />
+          <HStack justifyContent="space-between">
+            <PlatfromIconList
+              platform={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
         </CardBody>
       </Card>
     </>
