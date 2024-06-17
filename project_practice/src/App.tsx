be1a58d26d9caa2b0c96edf91e66d96834733b4c
cham1235 <hana171819@gmail.com>
2024-06-17 22:38:45 +0900

    facebook logo 追加

--- project_practice/src/App.tsx
+++ project_practice/src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
@@ -9,8 +10,8 @@ function App() {
           lg: '"nav nav nav" "menu aside right_menu"',
         }}
       >
-        <GridItem area={"nav"} bg={"green"}>
-          Nav
+        <GridItem area={"nav"} bg={"white"}>
+          <NavBar />
         </GridItem>
         <Show above="lg">
           <GridItem area={"menu"} bg={"pink"}>
