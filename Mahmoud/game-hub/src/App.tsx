cccdfe9cf2943a58e69f650ca07c2c976130de7d
Mahmoud-Da <mdabbagh6@gmail.com>
2024-06-25 15:31:55 +0900

    finish 22-_Highlighting_the_Selected_Genre

--- Mahmoud/game-hub/src/App.tsx
+++ Mahmoud/game-hub/src/App.tsx
@@ -25,7 +25,10 @@ function App() {
         </GridItem>
         <Show above="lg">
           <GridItem area={"aside"} paddingX="5px">
-            <GenreList onSelectedGenre={(genre) => setSelectedGenre(genre)} />
+            <GenreList
+              selectedGenre={selectedGenre}
+              onSelectedGenre={(genre) => setSelectedGenre(genre)}
+            />
           </GridItem>
         </Show>
         <GridItem area={"main"}>
