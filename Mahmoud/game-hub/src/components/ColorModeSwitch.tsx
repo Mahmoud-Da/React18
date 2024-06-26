bb987bd8282bd96cdf32cda1e7e171012245205e
Mahmoud-Da <mdabbagh6@gmail.com>
2024-06-26 13:41:05 +0900

    finish 30-_Building_Search_Input

--- Mahmoud/game-hub/src/components/ColorModeSwitch.tsx
+++ Mahmoud/game-hub/src/components/ColorModeSwitch.tsx
@@ -11,7 +11,7 @@ const ColorModeSwitch = () => {
           isChecked={colorMode == "dark"}
           onChange={toggleColorMode}
         ></Switch>
-        <Text>Dark Mode</Text>
+        <Text whiteSpace="nowrap">Dark Mode</Text>
       </HStack>
     </>
   );
