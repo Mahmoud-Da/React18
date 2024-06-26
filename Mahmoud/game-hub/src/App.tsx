eb52960fd22e5f97cf1fa97dbdf10a1ab8bd63de
Mahmoud-Da <mdabbagh6@gmail.com>
2024-06-26 11:47:06 +0900

    finish 27-_Sorting_Games

--- Mahmoud/game-hub/src/App.tsx
+++ Mahmoud/game-hub/src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from "./components/SortSelector";
 export interface GameQuery {
   genre: Genre | null;
   platform: Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -47,7 +48,12 @@ function App() {
                 setGameQuery({ ...gameQuery, platform })
               }
             ></PlatformSelector>
-            <SortSelector></SortSelector>
+            <SortSelector
+              sortOrder={gameQuery.sortOrder}
+              onSelectSortOrder={(sortOrder) =>
+                setGameQuery({ ...gameQuery, sortOrder })
+              }
+            ></SortSelector>
           </HStack>
           <GameGrid gameQurey={gameQuery} />
         </GridItem>
--- Mahmoud/game-hub/src/services/image-url.ts
+++ Mahmoud/game-hub/src/services/image-url.ts
@@ -1,4 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
+  // TODO: Cannot read properties of null (reading 'indexOf') for games without image
+  if (!url) return "";
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
