dea12c33aa72dad61174d2a74a8459aedc4f8f4b
Mahmoud-Da <mdabbagh6@gmail.com>
2024-08-01 11:42:58 +0900

    10-_Exercise-_Fetching_Data

--- Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/App.tsx
+++ Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
+import PostList from "./react-query/PostList";
 import TodoList from "./react-query/TodoList";
 
 function App() {
-  return <TodoList />;
+  return (
+    <>
+      <TodoList />
+      <PostList />
+    </>
+  );
 }
 
 export default App;
