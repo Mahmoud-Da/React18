2fc85e8c21e91c5282f9e5790bad888453acc379
Mahmoud-Da <mdabbagh6@gmail.com>
2024-08-01 11:27:21 +0900

    9-_Customizing_Query_Settings

--- Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/main.tsx
+++ Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/main.tsx
@@ -8,6 +8,20 @@ import "./index.css";
 
 const queryClient = new QueryClient();
 
+// change the qures defualt settigs globally
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       retry: 3,
+//       cacheTime: 300_000, // 5 minutes
+//       staleTime: 10 * 1000, // 10 seconds
+//       refetchOnWindowFocus: false,
+//       refetchOnReconnect: false,
+//       refetchOnMount: false,
+//     },
+//   },
+// });
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
--- Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/react-query/hooks/useTodos.ts
+++ Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/react-query/hooks/useTodos.ts
@@ -16,6 +16,7 @@ const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: ["todos"],
     queryFn: fetchTodos,
+    staleTime: 10 * 1000,
   });
 };
 
