9b6079bebaf517a6ef09796b56b8a7e1e33acb68
Mahmoud-Da <mdabbagh6@gmail.com>
2024-08-07 11:10:25 +0900

    12-_Paginated_Queries

--- Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/react-query/hooks/usePosts.ts
+++ Mahmoud/Fetching-and-Updating-Data-with-React-Query/react-course-part2-starter-main/src/react-query/hooks/usePosts.ts
@@ -7,21 +7,31 @@ interface Post {
   userId: number;
 }
 
-const usePosts = (userId: number | undefined) => {
+interface PostQuery {
+  userId: number | undefined;
+  page: number;
+  pageSize: number;
+}
+
+const usePosts = (query: PostQuery) => {
   const fetchPosts = () =>
     axios
       .get("https://jsonplaceholder.typicode.com/posts", {
         params: {
-          userId,
+          userId: query.userId,
+          _start: (query.page - 1) * query.pageSize,
+          _limit: query.pageSize,
         },
       })
       .then((res) => res.data);
 
   return useQuery<Post[], Error>({
     //  /users/1/posts
-    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    // queryKey: userId ? ["users", userId, "posts"] : ["posts"],
+    queryKey: ["posts", query],
     queryFn: fetchPosts,
     staleTime: 1 * 60 * 1000, // 1m
+    keepPreviousData: true,
   });
 };
 
