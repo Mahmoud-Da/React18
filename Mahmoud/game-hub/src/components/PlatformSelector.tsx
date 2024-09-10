43e78689664008432ef0a110afc6072b5b7dba25
Mahmoud-Da <mdabbagh6@gmail.com>
2024-09-10 12:02:32 +0900

    31-_Exercise-_Simplifying_Query_Objects

--- Mahmoud/game-hub/src/components/PlatformSelector.tsx
+++ Mahmoud/game-hub/src/components/PlatformSelector.tsx
@@ -3,12 +3,16 @@ import { BsChevronDown } from "react-icons/bs";
 import usePlatforms, { Platform } from "../hooks/usePlatforms";
 
 interface Props {
-  selectedPlatform: Platform | null;
   onSelectPlatform: (platform: Platform) => void;
+  selectedPlatformId?: number;
 }
 
-const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
+const PlatformSelector = ({ onSelectPlatform, selectedPlatformId }: Props) => {
   const { data, error } = usePlatforms();
+  const selectedPlatform = data?.results.find(
+    (p) => p.id === selectedPlatformId
+  );
+
   if (error) return null;
 
   return (
