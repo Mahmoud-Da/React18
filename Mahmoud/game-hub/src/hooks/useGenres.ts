7b450207abbf5f9fe5502e2caf98541830714a75
Mahmoud-Da <mdabbagh6@gmail.com>
2024-09-11 11:28:05 +0900

    32-_Exercise-_Creating_Lookup_Hooks

--- Mahmoud/game-hub/src/hooks/useGenres.ts
+++ Mahmoud/game-hub/src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import APIClient from "../services/api-client";
 import genres from "../data/genres";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 
@@ -15,7 +15,7 @@ const useGenres = () =>
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, // 24hours
-    initialData: { count: genres.length, results: genres },
+    initialData: genres,
   });
 
 export default useGenres;
