44bbbca298c32a9297428877699d81a323971f55
Mahmoud-Da <mdabbagh6@gmail.com>
2024-09-15 14:33:56 +0900

    2-_Consolidating_State_Logic_with_a_Reducer

--- Mahmoud/Global-State-Management/react-course-part2-starter-main/src/App.tsx
+++ Mahmoud/Global-State-Management/react-course-part2-starter-main/src/App.tsx
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
+import Counter from "./state-management/Counter";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return <Counter></Counter>;
 }
 
 export default App;
