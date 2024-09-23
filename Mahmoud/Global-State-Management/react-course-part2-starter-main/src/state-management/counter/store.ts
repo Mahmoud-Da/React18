6cd18ceef5e995b1f4adb22a8c857494f7d54297
Mahmoud-Da <mdabbagh6@gmail.com>
2024-09-24 04:02:52 +0900

    19-_Inspecting_Stores_with_Zustand_DevTools

--- Mahmoud/Global-State-Management/react-course-part2-starter-main/src/state-management/counter/store.ts
+++ Mahmoud/Global-State-Management/react-course-part2-starter-main/src/state-management/counter/store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
   counter: number;
@@ -13,5 +14,9 @@ const useCounterStore = create<CounterStore>((set) => ({
   increment: () => set((store) => ({ counter: store.counter + 1 })),
   reset: () => set(() => ({ max: 10 })),
 }));
+
+if (process.env.NODE_ENV == "development") {
+  mountStoreDevtool("Counter Store", useCounterStore);
+}
 
 export default useCounterStore;
