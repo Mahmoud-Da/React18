aa21611f8356bba744be4a6959dc921d9f148cd5
Mahmoud-Da <mdabbagh6@gmail.com>
2024-10-02 10:47:58 +0900

    6-_Nested_Routes

--- Mahmoud/Routing-with-React-Router/react-course-part2-starter-main/src/routing/routes.tsx
+++ Mahmoud/Routing-with-React-Router/react-course-part2-starter-main/src/routing/routes.tsx
@@ -1,25 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 import ContactPage from "./ContactPage";
 import HomePage from "./HomePage";
+import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/users",
-    element: <UserListPage />,
-  },
-  {
-    path: "/users/:id",
-    element: <UserDetailPage />,
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />,
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "users",
+        element: <UserListPage />,
+      },
+      {
+        path: "users/:id",
+        element: <UserDetailPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
   },
 ]);
 
