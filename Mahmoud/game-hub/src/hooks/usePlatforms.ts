ef3e634248bf2dc8164145cebd0f8c1b7693ad8f
Mahmoud-Da <mdabbagh6@gmail.com>
2024-10-12 21:55:31 +0900

    16-_Exercise-_Refactoring_Entities

--- Mahmoud/game-hub/src/hooks/usePlatforms.ts
+++ Mahmoud/game-hub/src/hooks/usePlatforms.ts
@@ -1,15 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
-import ms from "ms";
+import { Platform } from "../entity/Platform";
 
 const apiClient = new APIClient<Platform>("/platforms/lists/parents");
-
-export interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
 
 const usePlatforms = () =>
   useQuery({
